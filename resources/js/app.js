0e6297f5def163f2f624ae41b9d09e97882e41a0
jsierra14 <madesisierra@gmail.com>
2026-07-14 18:25:50 -0500

    Revert "03: CRUD projects - update (v2)"
    This reverts commit d12577c66723390325c84689de9ca7f2a01de4be.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,1 @@
 import "./bootstrap";
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('input[type="file"][data-preview]')
-        .forEach(input => {
-            input.addEventListener('change', function () {
-                const preview = document.getElementById(this.dataset.preview);
-                if (!preview || !this.files.length) {
-                    return;
-                }
-                const reader = new FileReader();
-                reader.onload = e => {
-                    preview.src = e.target.result;
-                    preview.classList.remove('hidden');
-                };
-                reader.readAsDataURL(this.files[0]);
-            });
-        });
-});
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -67,11 +67,11 @@
                         </td>
                         <td class="p-3">
                             <div class="flex justify-end gap-2">
-                                <a href="{{ route('admin.projects.edit', $project) }}"
+                                <a href=""
                                     class="px-3 py-1 rounded-md border border-gray-300 dark:border-gray-700">Editar</a>
                                 <form
                                     method="POST"
-                                    action="{{ route('admin.projects.destroy', $project) }}">
+                                    action="#">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit"
