d12577c66723390325c84689de9ca7f2a01de4be
jsierra14 <madesisierra@gmail.com>
2026-07-14 18:20:35 -0500

    03: CRUD projects - update (v2)

--- database/migrations/2026_07_13_173906_create_project_service.php
+++ database/migrations/2026_07_13_173906_create_project_service.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('project_services', function (Blueprint $table) {
+        Schema::create('project_service', function (Blueprint $table) {
             $table->id();
             $table->foreignId('project_id')->constrained()->onDelete('cascade');
             $table->foreignId('service_id')->constrained()->onDelete('cascade');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('project_services');
+        Schema::dropIfExists('project_service');
     }
 };
